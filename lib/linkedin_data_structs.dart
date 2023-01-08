a3a0f19f34bfa7ea16593a4410ee0d9af87c3e49
bjadczak <73365815+bjadczak@users.noreply.github.com>
2023-01-08 18:02:28 +0100

    Add CVData class

--- lib/linkedin_data_structs.dart
+++ lib/linkedin_data_structs.dart
@@ -1,4 +1,6 @@
 import 'package:intl/intl.dart';
+import 'package:csv/csv.dart';
+import 'dart:io';
 
 class PositionData {
   final String companyName;
@@ -49,18 +51,20 @@ class ProfileData {
   final String headLine;
   final String industry;
   final String location;
+  final String email;
 
   ProfileData(this.firstName, this.secondName, this.headLine, this.industry,
-      this.location);
+      this.location, this.email);
   ProfileData.empty()
       : firstName = "",
         secondName = "",
         headLine = "",
         industry = "",
-        location = "";
+        location = "",
+        email = "";
   void debugPrint() {
     print(
-        "Profile of ${firstName} ${secondName}. ${headLine} ${industry} ${location}");
+        "Profile of ${firstName} ${secondName}. ${headLine} ${industry} ${location}. Email: ${email}");
   }
 }
 
@@ -120,4 +124,155 @@ class SkillData {
 
   @override
   int get hashCode => skill.hashCode;
+}
+
+class CvData {
+  late final List<PositionData> positions;
+  late final List<EducationData> education;
+  late final List<SkillData> skills;
+  late final ProfileData profileData;
+
+  CvData._(this.positions, this.education, this.skills, this.profileData);
+
+  static Future<CvData> create(Directory unpackedCsvFiles) async {
+    var positions = await _parsePositions(unpackedCsvFiles);
+    var profileData = await _parseProfile(unpackedCsvFiles);
+    var education = await _parseEducation(unpackedCsvFiles);
+    var skills = await _parseSkills(unpackedCsvFiles);
+
+    return CvData._(positions, education, skills, profileData);
+  }
+
+  void debugPrint() {
+    profileData.debugPrint();
+    for (var pos in positions) {
+      pos.debugPrint();
+    }
+    for (var edu in education) {
+      edu.debugPrint();
+    }
+    for (var skill in skills) {
+      skill.debugPrint();
+    }
+  }
+
+  static Future<List<PositionData>> _parsePositions(
+      Directory unpackedCsvFiles) async {
+    File positionsCsv = File("${unpackedCsvFiles.path}/Positions.csv");
+
+    if (!(await positionsCsv.exists())) {
+      return [];
+    }
+    List<PositionData> outData = [];
+
+    try {
+      List<List<dynamic>> rowsAsListOfValues =
+          await _getListFromCSV(positionsCsv);
+
+      // First line is an header
+      for (var row
+          in rowsAsListOfValues.getRange(1, rowsAsListOfValues.length)) {
+        outData
+            .add(PositionData(row[0], row[1], row[2], row[3], row[4], row[5]));
+      }
+    } on Exception catch (_) {}
+
+    return outData;
+  }
+
+  static Future<ProfileData> _parseProfile(Directory unpackedCsvFiles) async {
+    File profileCsv = File("${unpackedCsvFiles.path}/Profile.csv");
+    File emailCsv = File("${unpackedCsvFiles.path}/Email Addresses.csv");
+
+    if (!(await profileCsv.exists()) && !(await emailCsv.exists())) {
+      return ProfileData.empty();
+    }
+    ProfileData outData;
+    try {
+      List<List<dynamic>> rowsAsListOfValuesProfile =
+          await _getListFromCSV(profileCsv);
+      List<List<dynamic>> rowsAsListOfValuesEmail =
+          await _getListFromCSV(emailCsv);
+
+      // First line is an header
+      var row = rowsAsListOfValuesProfile[1];
+      String email;
+      try {
+        email = rowsAsListOfValuesEmail.firstWhere(
+          (element) => element[1] == "Yes" && element[2] == "Yes",
+          orElse: () => [""],
+        )[0];
+      } on Exception catch (_) {
+        email = "";
+      }
+
+      outData = ProfileData(row[0], row[1], row[5], row[7], row[9], email);
+    } on Exception catch (_) {
+      outData = ProfileData.empty();
+    }
+
+    return outData;
+  }
+
+  static Future<List<EducationData>> _parseEducation(
+      Directory unpackedCsvFiles) async {
+    File educationCSV = File("${unpackedCsvFiles.path}/Education.csv");
+
+    if (!(await educationCSV.exists())) {
+      return [];
+    }
+    List<EducationData> outData = [];
+
+    try {
+      List<List<dynamic>> rowsAsListOfValues =
+          await _getListFromCSV(educationCSV);
+
+      // First line is an header
+      for (var row
+          in rowsAsListOfValues.getRange(1, rowsAsListOfValues.length)) {
+        outData.add(EducationData(row[0], row[1], row[2], row[4]));
+      }
+    } on Exception catch (_) {}
+
+    return outData;
+  }
+
+  static Future<List<SkillData>> _parseSkills(
+      Directory unpackedCsvFiles) async {
+    File skillsCSV = File("${unpackedCsvFiles.path}/Skills.csv");
+    File endorsementCSV =
+        File("${unpackedCsvFiles.path}/Endorsement_Received_Info.csv");
+
+    if (!(await skillsCSV.exists()) && !(await endorsementCSV.exists())) {
+      return [];
+    }
+    List<SkillData> outData = [];
+
+    try {
+      List<List<dynamic>> rowsAsListOfValuesSkills =
+          await _getListFromCSV(skillsCSV);
+      List<List<dynamic>> rowsAsListOfValuesEndorsment =
+          await _getListFromCSV(endorsementCSV);
+
+      // First line is an header
+      for (var row in rowsAsListOfValuesSkills.getRange(
+          1, rowsAsListOfValuesSkills.length)) {
+        outData.add(SkillData(row[0]));
+      }
+      for (var row in rowsAsListOfValuesEndorsment.getRange(
+          1, rowsAsListOfValuesEndorsment.length)) {
+        SkillData skill = SkillData(row[1]);
+        if (row[4] == "ACCEPTED" && !outData.contains(skill)) {
+          outData.add(skill);
+        }
+      }
+    } on Exception catch (_) {}
+
+    return outData;
+  }
+
+  static Future<List<List>> _getListFromCSV(File csvFile) async {
+    return const CsvToListConverter(eol: "\n", shouldParseNumbers: false)
+        .convert(await csvFile.readAsString());
+  }
 }
--- lib/load_data_screen.dart
+++ lib/load_data_screen.dart
@@ -7,8 +7,6 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:flutter_archive/flutter_archive.dart';
 
-import 'package:csv/csv.dart';
-
 import 'linkedin_data_structs.dart';
 
 class ThirdRoute extends StatelessWidget {
@@ -29,134 +27,11 @@ class ThirdRoute extends StatelessWidget {
         File file = File(zipFilePath);
         Directory? unpackedFiles = await _getFilesFromZip(file);
         if (unpackedFiles != null) {
-          await _parseLinkedInData(unpackedFiles);
+          CvData data = await CvData.create(unpackedFiles);
+          data.debugPrint();
         }
       }
     }
-  }
-
-  Future<void> _parseLinkedInData(Directory unpackedCsvFiles) async {
-    var postions = await _parsePositions(unpackedCsvFiles);
-    var profile = await _parseProfile(unpackedCsvFiles);
-    var educations = await _parseEducation(unpackedCsvFiles);
-    var skills = await _parseSkills(unpackedCsvFiles);
-
-    profile.debugPrint();
-    for (var position in postions) {
-      position.debugPrint();
-    }
-    for (var education in educations) {
-      education.debugPrint();
-    }
-    for (var skill in skills) {
-      skill.debugPrint();
-    }
-  }
-
-  Future<List<PositionData>> _parsePositions(Directory unpackedCsvFiles) async {
-    File positionsCsv = File("${unpackedCsvFiles.path}/Positions.csv");
-
-    if (!(await positionsCsv.exists())) {
-      return [];
-    }
-    List<PositionData> outData = [];
-
-    try {
-      List<List<dynamic>> rowsAsListOfValues =
-          await _getListFromCSV(positionsCsv);
-
-      // First line is an header
-      for (var row
-          in rowsAsListOfValues.getRange(1, rowsAsListOfValues.length)) {
-        outData
-            .add(PositionData(row[0], row[1], row[2], row[3], row[4], row[5]));
-      }
-    } on Exception catch (_) {}
-
-    return outData;
-  }
-
-  Future<ProfileData> _parseProfile(Directory unpackedCsvFiles) async {
-    File profileCsv = File("${unpackedCsvFiles.path}/Profile.csv");
-
-    if (!(await profileCsv.exists())) {
-      return ProfileData.empty();
-    }
-    ProfileData outData;
-    try {
-      List<List<dynamic>> rowsAsListOfValues =
-          await _getListFromCSV(profileCsv);
-
-      // First line is an header
-      var row = rowsAsListOfValues[1];
-
-      outData = ProfileData(row[0], row[1], row[5], row[7], row[9]);
-    } on Exception catch (_) {
-      outData = ProfileData.empty();
-    }
-
-    return outData;
-  }
-
-  Future<List<EducationData>> _parseEducation(
-      Directory unpackedCsvFiles) async {
-    File educationCSV = File("${unpackedCsvFiles.path}/Education.csv");
-
-    if (!(await educationCSV.exists())) {
-      return [];
-    }
-    List<EducationData> outData = [];
-
-    try {
-      List<List<dynamic>> rowsAsListOfValues =
-          await _getListFromCSV(educationCSV);
-
-      // First line is an header
-      for (var row
-          in rowsAsListOfValues.getRange(1, rowsAsListOfValues.length)) {
-        outData.add(EducationData(row[0], row[1], row[2], row[4]));
-      }
-    } on Exception catch (_) {}
-
-    return outData;
-  }
-
-  Future<List<SkillData>> _parseSkills(Directory unpackedCsvFiles) async {
-    File skillsCSV = File("${unpackedCsvFiles.path}/Skills.csv");
-    File endorsementCSV =
-        File("${unpackedCsvFiles.path}/Endorsement_Received_Info.csv");
-
-    if (!(await skillsCSV.exists()) && !(await endorsementCSV.exists())) {
-      return [];
-    }
-    List<SkillData> outData = [];
-
-    try {
-      List<List<dynamic>> rowsAsListOfValuesSkills =
-          await _getListFromCSV(skillsCSV);
-      List<List<dynamic>> rowsAsListOfValuesEndorsment =
-          await _getListFromCSV(endorsementCSV);
-
-      // First line is an header
-      for (var row in rowsAsListOfValuesSkills.getRange(
-          1, rowsAsListOfValuesSkills.length)) {
-        outData.add(SkillData(row[0]));
-      }
-      for (var row in rowsAsListOfValuesEndorsment.getRange(
-          1, rowsAsListOfValuesEndorsment.length)) {
-        SkillData skill = SkillData(row[1]);
-        if (row[4] == "ACCEPTED" && !outData.contains(skill)) {
-          outData.add(skill);
-        }
-      }
-    } on Exception catch (_) {}
-
-    return outData;
-  }
-
-  Future<List<List>> _getListFromCSV(File csvFile) async {
-    return const CsvToListConverter(eol: "\n", shouldParseNumbers: false)
-        .convert(await csvFile.readAsString());
   }
 
   Future<Directory?> _getFilesFromZip(File myZipData) async {
